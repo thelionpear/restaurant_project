88cdf4adb11bae27196caf48c0c591b629754dcd
Michelle Garcia <thelionpear@gmail.com>
2018-05-16 17:20:45 -0600

    initial commit but also a lot is working

---
+++ main.js
@@ -0,0 +1,28 @@
+
+$("#appetizers").mouseenter(function() {
+    $(".appetizer_choices").removeClass("hide_flex");
+    $(".appetizer_choices").addClass("show_flex");
+    $(".appetizer_choices").slideDown(); 
+});
+$("#appetizers").mouseleave(function() {
+    $(".appetizer_choices").slideUp();
+});
+$("#entrees").mouseenter(function() {
+    $(".entree_choices").removeClass("hide_flex");
+    $(".entree_choices").addClass("show_flex");
+    $(".entree_choices").slideDown();
+});
+$("#entrees").mouseleave(function() { 
+    $(".entree_choices").slideUp(); 
+});
+
+$("#desserts").mouseenter(function() {
+    $(".dessert_choices").removeClass("hide_flex"); 
+    $(".dessert_choices").addClass("show_flex");
+    $(".dessert_choices").slideDown();
+});
+$("#desserts").mouseleave(function() {
+    $(".dessert_choices").slideUp();
+});
+
+
