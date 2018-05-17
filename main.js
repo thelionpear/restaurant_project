65dd723e7358bd9383679dd1a27a19842fd46aaa
Michelle Garcia <thelionpear@gmail.com>
2018-05-17 09:18:06 -0600

    css changes

--- main.js
+++ main.js
@@ -1,28 +1,32 @@
+function start() {
+    $("#appetizers").mouseenter(function() {
+        $(".appetizer_choices").removeClass("hide_flex");
+        $(".appetizer_choices").addClass("show_flex");
+        $(".appetizer_choices").slideDown(); 
+    });
+    $("#appetizers").mouseleave(function() {
+        $(".appetizer_choices").slideUp();
+    });
+    $("#entrees").mouseenter(function() {
+        $(".entree_choices").removeClass("hide_flex");
+        $(".entree_choices").addClass("show_flex");
+        $(".entree_choices").slideDown();
+    });
+    $("#entrees").mouseleave(function() { 
+        $(".entree_choices").slideUp(); 
+    });
+    
+    $("#desserts").mouseenter(function() {
+        $(".dessert_choices").removeClass("hide_flex"); 
+        $(".dessert_choices").addClass("show_flex");
+        $(".dessert_choices").slideDown();
+    });
+    $("#desserts").mouseleave(function() {
+        $(".dessert_choices").slideUp();
+    });
+}
 
-$("#appetizers").mouseenter(function() {
-    $(".appetizer_choices").removeClass("hide_flex");
-    $(".appetizer_choices").addClass("show_flex");
-    $(".appetizer_choices").slideDown(); 
-});
-$("#appetizers").mouseleave(function() {
-    $(".appetizer_choices").slideUp();
-});
-$("#entrees").mouseenter(function() {
-    $(".entree_choices").removeClass("hide_flex");
-    $(".entree_choices").addClass("show_flex");
-    $(".entree_choices").slideDown();
-});
-$("#entrees").mouseleave(function() { 
-    $(".entree_choices").slideUp(); 
-});
+start()
 
-$("#desserts").mouseenter(function() {
-    $(".dessert_choices").removeClass("hide_flex"); 
-    $(".dessert_choices").addClass("show_flex");
-    $(".dessert_choices").slideDown();
-});
-$("#desserts").mouseleave(function() {
-    $(".dessert_choices").slideUp();
-});
 
 
